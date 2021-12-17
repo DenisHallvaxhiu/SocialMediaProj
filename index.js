f02155ba0975d7c2fc8ee216c0eb21dd0f75863f
jimmytran16 <jimmytran1620@gmail.com>
2021-12-16 21:39:54 -0500

    login passport set up

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const app = express()
 const port =  process.env.PORT || 3000
+const flash = require('connect-flash')
 const connection = require('./database');
+const passport = require('passport');
+require('./passport')(passport); // pass passport for configuration
+const session = require('express-session');
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded())
+
+// passport configurations
+app.use(session({ secret: 'thisissecret' }))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 app.set('view engine', 'ejs');
+
+
+
 
 app.get('/', (req, res) => {
   res.render('login', { message: '' });
 })
 
 app.get('/register', (req, res) => {
+  console.log(req.user)
   res.render('register');
 })
 
 app.get('/dashboard', (req, res) => {
+  console.log(req.user);
   // data: pretend like this is the data that is returned after we called it from the database
   // data: this is an array that contains objects which represents data of a particular post
   const data = [
@@ -63,10 +79,12 @@ app.post('/registerUser',async(req,res)=>{
   let lastName = req.body.lName
   let email = req.body.email
   let password = req.body.password
-  const hashedPassword = await bcrypt.hash(password,10);
+  // const hashedPassword = await bcrypt.hash(password,10);
+  const hashedPassword = password;
 
   try {
     connection.query("SELECT username FROM user WHERE username = ?",[userName],(err,result,fields)=>{
+      if (err) console.log(err);
       if (result.length >0){
           console.log(userName +" exists")
       }
@@ -89,34 +107,44 @@ app.post('/registerUser',async(req,res)=>{
   }
 })
 
-app.post('/loginUser',async (req, res) => {
+// app.post('/loginUser',async (req, res) => {
 
-  let username = req.body.username
-  let password = req.body.password
+//   let username = req.body.username
+//   let password = req.body.password
 
-  // logic that authrencates the user
-  // call thee database
-  // authenticate if user is existing in the database
-  try {
-    connection.query("SELECT password FROM user WHERE userName = ?",[username],async(err,result,fields)=>{
-      if(err){
-         console.log(err)
-      }
-      if (await bcrypt.compare(password,result[0].password)){
-          console.log(result[0].password)
-          console.log("exists")
-          res.redirect("/dashboard")
-      }else
-        {
-          console.log(result)
-          res.render('login', { message: 'The username does not exist' })
-        }
-      })
-  } catch (error) {
+//   // logic that authrencates the user
+//   // call thee database
+//   // authenticate if user is existing in the database
+//   try {
+//     connection.query("SELECT password FROM user WHERE userName = ?",[username],async(err,result,fields)=>{
+//       if(err){
+//          console.log(err)
+//       }
+//       if (await bcrypt.compare(password,result[0].password)){
+//           console.log(result[0].password)
+//           console.log("exists")
+//           res.redirect("/dashboard")
+//       }else
+//         {
+//           console.log(result)
+//           res.render('login', { message: 'The username does not exist' })
+//         }
+//       })
+//   } catch (error) {
     
-  }
+//   }
  
-})
+// })
+
+// app.use('/loginUser', (req,res) => {
+
+// })
+
+app.post('/loginUser', passport.authenticate('local-login', {
+  successRedirect : '/dashboard', // redirect to the secure profile section
+  failureRedirect : '/', // redirect back to the signup page if there is an error
+  failureFlash : true // allow flash messages
+}));
 
 
 app.listen(port, () => {
