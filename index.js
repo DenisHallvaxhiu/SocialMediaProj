77e078a144f7180ecbdae423e6eea670729d890b
jimmytran16 <jimmytran1620@gmail.com>
2021-12-17 20:54:37 -0500

    passport

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
+// DEPENDENCIES
 const express = require('express')
 const bcrypt = require('bcrypt')
 const app = express()
-const port =  process.env.PORT || 3000
-const flash = require('connect-flash')
+const port = process.env.PORT || 3000
 const connection = require('./database');
+const middlewares = require('./middlewares/authenticate');
+// passport dependencies
 const passport = require('passport');
-require('./passport')(passport); // pass passport for configuration
+const flash = require('connect-flash')
 const session = require('express-session');
+require('./passport')(passport); // pass passport for configuration
 
+
+// MIDDLEWARES
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded())
 
-// passport configurations
+// passport middlewares
 app.use(session({ secret: 'thisissecret' }))
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash()); // use connect-flash for flash messages stored in session
+app.use((req,res,next) => {
+  console.log(req.user)
+  console.log(req.session)
+  next();
+})
 
-app.set('view engine', 'ejs');
-
-
-
-
+// ROUTES
 app.get('/', (req, res) => {
   res.render('login', { message: '' });
 })
@@ -32,7 +39,7 @@ app.get('/register', (req, res) => {
   res.render('register');
 })
 
-app.get('/dashboard', (req, res) => {
+app.get('/dashboard', middlewares.isLoggedIn, (req, res) => {
   console.log(req.user);
   // data: pretend like this is the data that is returned after we called it from the database
   // data: this is an array that contains objects which represents data of a particular post
@@ -57,7 +64,7 @@ app.get('/dashboard', (req, res) => {
       postedBy: 'jimmytran16@gmail.com',
       profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
       mood: 'Happy'
-    },{
+    }, {
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
       date: 'December 22, 2021',
       postedBy: 'jimmytran16@gmail.com',
@@ -73,78 +80,45 @@ app.get('/profile', (req, res) => {
   res.render('profile');
 })
 
-app.post('/registerUser',async(req,res)=>{
+app.post('/registerUser', async (req, res) => {
   let userName = req.body.userName
   let firstName = req.body.fName
   let lastName = req.body.lName
   let email = req.body.email
   let password = req.body.password
-  // const hashedPassword = await bcrypt.hash(password,10);
-  const hashedPassword = password;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    connection.query("SELECT username FROM user WHERE username = ?",[userName],(err,result,fields)=>{
+    connection.query("SELECT username FROM user WHERE username = ?", [userName], (err, result, fields) => {
       if (err) console.log(err);
-      if (result.length >0){
-          console.log(userName +" exists")
+      if (result.length > 0) {
+        console.log(userName + " exists")
       }
-      else
-        {
-          connection.query("INSERT INTO  user (`username`, `password`, `firstName`, `lastName`, `email`) VALUES ('"+userName+"','"+hashedPassword+"','"+firstName+"','"+lastName+"','"+email+"')"
-         ,(err,result)=>{
-          if(err){
-            console.log(err)
-          }
-          else{
-            console.log(userName + " registerd")
-            res.redirect("/")
-          }
+      else {
+        connection.query("INSERT INTO  user (`username`, `password`, `firstName`, `lastName`, `email`) VALUES ('" + userName + "','" + hashedPassword + "','" + firstName + "','" + lastName + "','" + email + "')"
+          , (err, result) => {
+            if (err) {
+              console.log(err)
+            }
+            else {
+              console.log(userName + " registerd")
+              res.redirect("/")
+            }
           });
-        }
-      })
+      }
+    })
   } catch (error) {
-    
+
   }
 })
 
-// app.post('/loginUser',async (req, res) => {
-
-//   let username = req.body.username
-//   let password = req.body.password
-
-//   // logic that authrencates the user
-//   // call thee database
-//   // authenticate if user is existing in the database
-//   try {
-//     connection.query("SELECT password FROM user WHERE userName = ?",[username],async(err,result,fields)=>{
-//       if(err){
-//          console.log(err)
-//       }
-//       if (await bcrypt.compare(password,result[0].password)){
-//           console.log(result[0].password)
-//           console.log("exists")
-//           res.redirect("/dashboard")
-//       }else
-//         {
-//           console.log(result)
-//           res.render('login', { message: 'The username does not exist' })
-//         }
-//       })
-//   } catch (error) {
-    
-//   }
- 
-// })
-
-// app.use('/loginUser', (req,res) => {
-
-// })
-
 app.post('/loginUser', passport.authenticate('local-login', {
-  successRedirect : '/dashboard', // redirect to the secure profile section
-  failureRedirect : '/', // redirect back to the signup page if there is an error
-  failureFlash : true // allow flash messages
+  successRedirect: '/dashboard', // redirect to the secure profile section
+  failureRedirect: '/', // redirect back to the signup page if there is an error
+  failureFlash: true // allow flash messages
 }));
+
+// ROUTES END
 
 
 app.listen(port, () => {
--- passport.js
+++ passport.js
@@ -1,6 +1,12 @@
 // load all the things we need
 var LocalStrategy = require('passport-local').Strategy;
 var connection = require('./database');
+var bcrypt = require('bcrypt');
+
+
+// FLOW OF THE PASSPORT LOGIC :
+// https://www.bing.com/images/search?view=detailV2&ccid=AewBtooE&id=9042544C74C647E9D3395E12D6D33DF442AA39B5&thid=OIP.AewBtooEVhDs1PTQOaIQSgHaFO&mediaurl=https%3a%2f%2fi.pinimg.com%2foriginals%2f45%2f34%2f16%2f4534166722a822c4b8335504ee2b6fd0.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.01ec01b68a045610ecd4f4d039a2104a%3frik%3dtTmqQvQ909YSXg%26pid%3dImgRaw%26r%3d0&exph=441&expw=624&q=flow+of+passport+local+strategy+&simid=608016895124047387&FORM=IRPRST&ck=5DC20969C1A01C44EE05484BA8252EEC&selectedIndex=0&ajaxhist=0&ajaxserp=0
+
 
 // expose this function to our app using module.exports
 module.exports = function (passport) {
@@ -16,10 +22,10 @@ module.exports = function (passport) {
         done(null, user.username);
     });
 
-    // used to deserialize the user
+    // used to deserialize the user - to store into the req.user 
     passport.deserializeUser(function (username, done) {
         connection.query('SELECT * FROM User WHERE username = ?', [username], (err, user) => {
-            done(err, user[0])
+            done(err, { username: user[0].username, firstName: user[0].firstName, lastName: user[0].lastName })
         })
     });
 
@@ -36,12 +42,12 @@ module.exports = function (passport) {
         passwordField : 'password',
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
-    function(req, email, password, done) { // callback with email and password from our form
+    function (req, email, password, done) { // callback with email and password from our form
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
 
-        connection.query('SELECT * FROM User WHERE username = ?', [email], (err, user) => {
+        connection.query('SELECT * FROM User WHERE username = ?', [email], async (err, user) => {
             var user = user[0];
             // if there are any errors, return the error before anything else
             if (err)
@@ -52,7 +58,9 @@ module.exports = function (passport) {
                 return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 
             // if the user is found but the password is wrong
-            if (user.password !== password)
+            var isValid = await bcrypt.compare(password,user.password);
+            
+            if (!isValid)
                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
             // all is well, return successful user
@@ -61,6 +69,5 @@ module.exports = function (passport) {
 
 
     }));
-
 };
 
