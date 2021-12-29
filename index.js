90eff4385a76a36061dcaa4dfbda6e7dc2a43d78
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2021-12-28 23:35:25 -0500

    post_updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const middlewares = require('./middlewares/authenticate');
 const passport = require('passport');
 const flash = require('connect-flash')
 const session = require('express-session');
+const { redirect } = require('express/lib/response');
 require('./passport')(passport); // pass passport for configuration
 
 
@@ -43,37 +44,48 @@ app.get('/dashboard', middlewares.isLoggedIn, (req, res) => {
   console.log(req.user);
   // data: pretend like this is the data that is returned after we called it from the database
   // data: this is an array that contains objects which represents data of a particular post
-  const posts = [
-    {
-      description: "Lorem Ipsum is industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leapLorem Ipsum is industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-      date: 'May 22, 2021',
-      postedBy: 'Denis@gmail.com',
-      profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-      mood: 'Sad'
-    },
-    {
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-      date: 'April 20, 2021',
-      postedBy: 'Khizer@gmail.com',
-      profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-      mood: 'Happy'
-    },
-    {
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-      date: 'November 20, 2021',
-      postedBy: 'jimmytran16@gmail.com',
-      profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-      mood: 'Happy'
-    }, {
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-      date: 'December 22, 2021',
-      postedBy: 'jimmytran16@gmail.com',
-      profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-      mood: 'Happy'
+  
+  connection.query("SELECT * FROM post ORDER BY ID DESC",(err, result)=>{
+   if(err)
+    redirect("/dashboard")
+    else{
+      const posts = result;
+      res.render('dashboard', { posts: posts });
     }
-  ]
+  })
+  
+  
+  
+  // const posts = [
+  //   {
+  //     description: "Lorem Ipsum is industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leapLorem Ipsum is industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+  //     date: 'May 22, 2021',
+  //     postedBy: 'Denis@gmail.com',
+  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
+  //     mood: 'Sad'
+  //   },
+  //   {
+  //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+  //     date: 'April 20, 2021',
+  //     postedBy: 'Khizer@gmail.com',
+  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
+  //     mood: 'Happy'
+  //   },
+  //   {
+  //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+  //     date: 'November 20, 2021',
+  //     postedBy: 'jimmytran16@gmail.com',
+  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
+  //     mood: 'Happy'
+  //   }, {
+  //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+  //     date: 'December 22, 2021',
+  //     postedBy: 'jimmytran16@gmail.com',
+  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
+  //     mood: 'Happy'
+  //   }
+  // ]
   // we will pass in a variable called 'posts' that will have the value of 'data' to the ejs/HTML webpage
-  res.render('dashboard', { posts: posts });
 })
 
 // Endpoint to handle the creation of a user's post
@@ -105,6 +117,8 @@ app.post('/createPost', (req, res) => {
 
         }
   // Query to save the information
+
+  
   // redirect back to the dashbard
 })
 
