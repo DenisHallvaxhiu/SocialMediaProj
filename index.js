9115f8d9b160fa2d3e31a8f661aaeb990ff1ac7f
Denis Hallvaxhiu <dhallvaxhiu999@gmail.com>
2022-01-02 20:44:42 -0500

    profile Update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const connection = require('./database');
 const middlewares = require('./middlewares/authenticate');
+const loggedIn = require('./middlewares/isLoggedIn');
 // passport dependencies
 const passport = require('passport');
 const flash = require('connect-flash')
@@ -23,15 +24,11 @@ app.use(express.urlencoded())
 app.use(session({ secret: 'thisissecret' }))
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash()); // use connect-flash for flash messages stored in session
-// app.use((req,res,next) => {
-//   console.log(req.user)
-//   console.log(req.session)
-//   next();
-// })
+app.use(flash()); 
+
 
 // ROUTES
-app.get('/', (req, res) => {
+app.get('/',loggedIn.isLoggedIn, (req, res) => {
   res.render('login', { message: '' });
 })
 
@@ -40,11 +37,14 @@ app.get('/register', (req, res) => {
   res.render('register');
 })
 
+app.get('/logOut',middlewares.isAuthenticate,(req,res)=>{
+  req.logOut();
+  res.redirect("/")
+})
 
-app.get('/dashboard', middlewares.isLoggedIn, (req, res) => {
+
+app.get('/dashboard', middlewares.isAuthenticate, (req, res) => {
   console.log(req.user);
-  // data: pretend like this is the data that is returned after we called it from the database
-  // data: this is an array that contains objects which represents data of a particular post
 
   connection.query("SELECT * FROM post ORDER BY ID DESC", (err, result) => {
     if (err)
@@ -54,43 +54,10 @@ app.get('/dashboard', middlewares.isLoggedIn, (req, res) => {
       res.render('dashboard', { posts: posts });
     }
   })
-
-
-
-  // const posts = [
-  //   {
-  //     description: "Lorem Ipsum is industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leapLorem Ipsum is industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-  //     date: 'May 22, 2021',
-  //     postedBy: 'Denis@gmail.com',
-  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-  //     mood: 'Sad'
-  //   },
-  //   {
-  //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-  //     date: 'April 20, 2021',
-  //     postedBy: 'Khizer@gmail.com',
-  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-  //     mood: 'Happy'
-  //   },
-  //   {
-  //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-  //     date: 'November 20, 2021',
-  //     postedBy: 'jimmytran16@gmail.com',
-  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-  //     mood: 'Happy'
-  //   }, {
-  //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
-  //     date: 'December 22, 2021',
-  //     postedBy: 'jimmytran16@gmail.com',
-  //     profileUrl: 'https://cdn4.iconfinder.com/data/icons/user-people-2/48/6-512.png',
-  //     mood: 'Happy'
-  //   }
-  // ]
-  // we will pass in a variable called 'posts' that will have the value of 'data' to the ejs/HTML webpage
 })
 
 // Endpoint to handle the creation of a user's post
-app.post('/createPost', (req, res) => {
+app.post('/createPost',middlewares.isAuthenticate, (req, res) => {
   console.log(req.body);
   console.log('the session', req.session.passport.user);
   // Extract the emotion, thoughts, and the username 
@@ -117,13 +84,10 @@ app.post('/createPost', (req, res) => {
   catch (e) {
 
   }
-  // Query to save the information
 
-
-  // redirect back to the dashbard
 })
 
-app.get('/profile', (req, res) => {
+app.get('/profile',middlewares.isAuthenticate, (req, res) => {
 
   let username = req.session.passport.user;
 
@@ -135,6 +99,17 @@ app.get('/profile', (req, res) => {
       res.render('profile', { posts: posts });
     }
   })
+})
+
+app.get('/deletePost/:postId',middlewares.isAuthenticate,function(req,res){
+    connection.query("delete from post where id =" + req.params.postId,(err,result)=>{
+      if(err){
+        res.render('profile', { posts: [], message: 'There was an error deleting the post' });
+      }
+      else{
+        res.redirect("/profile");
+      }
+    })
 })
 
 app.post('/registerUser', async (req, res) => {
